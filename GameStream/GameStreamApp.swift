4db9983b0d773acd8d2f6c1f0ba7c33301ba706d
Crist Gimenez <mail@cri.st>
2022-11-07 21:50:17 -0300

    Initial project commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Crist on 07/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
